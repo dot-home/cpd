3ed24ab01a6d6891910468b0a55071b97ad21815
Curt J. Sampson <cjs@cynic.net>
2018-03-18 14:55:08 +0900

    Glob matches closer to the end of the path are now considered better
    We introduce new MatchingPath class, encapsulating the path and a
    sorty key, to handle matching and sorting. This uses basically the
    same code as before to deal with figuring out which path components
    match globs, but we add sorting based on rightmost matches taking
    priority.
    
    Two MatchingPaths with different sets of globs to match are considered
    incomparable.

--- bin/cpd.py
+++ bin/cpd.py
@@ -2,7 +2,7 @@
 
 from argparse import ArgumentParser
 from fnmatch import fnmatch
-from functools import reduce
+from functools import reduce, total_ordering
 from glob import glob
 from os import environ
 from os.path import expanduser, split as pathsplit
@@ -14,6 +14,9 @@ def flatten(xs):
     #   Monads are handy!  https://stackoverflow.com/a/952946/107294
     return sum(map(list, xs), [])
 
+def reverse(s):
+    return s[::-1]
+
 def readconfig():
     with open(expanduser(project_path_globs_file), 'r') as f:
         return map(str.strip, f.readlines())
@@ -24,27 +27,89 @@ def expand_pdglob(pdglob):
     '''
     return glob(expanduser(pdglob))
 
-def pdmatches(component_globs):     # !!! Curried
-    ''' Return a function that will return all paths from `project_paths`
-        that have components matching all `component_globs`. Each
-        component_glob has an implied `*` at the end.
-    '''
-    def match_paths(project_paths):
-        return filter(lambda path: all(glob_match(path, glob)
-                                       for glob in component_globs),
-                      project_paths)
-    return match_paths
-
-def glob_match(path, glob):
-    return any(fnmatch(component, glob + '*')
-               for component in pathcomponents(path))
-
 def pathcomponents(path):
     prefix, component = pathsplit(path)
     if prefix not in ['', '/']:         # Fixed point
         return pathcomponents(prefix) + [component]
     else:
         return [component]
+
+@total_ordering
+class MatchingPath():
+    ''' A path that sorts in 'best match' order. '''
+
+    class AllGlobsMustMatch(RuntimeError):      pass
+    class Incomparable(RuntimeError):           pass
+
+    @staticmethod
+    def constructor(component_globs):
+        ''' Return a constructor function that builds MatchingPath
+            objects. The constructor takes and returns a MatchingPath
+            if all `component_globs` globs match at least one path
+            component, or `None` otherwise.
+        '''
+        def cons(path):
+            try:
+                return MatchingPath(path, component_globs)
+            except MatchingPath.AllGlobsMustMatch:
+                return None
+        return cons
+
+    def __init__(self, path, component_globs):
+        self.path = path
+        self.cglobs = set(component_globs)
+        self.sortkey = self.makesortkey(component_globs)(path)
+
+    @staticmethod
+    def makesortkey(component_globs):
+        ''' Take list of component_globs and (curried) path string and return
+            a tuple of the path and the match data. If every glob matched at
+            least one component, the match data is a string of `0` and `1`
+            indicating which components matched any glob. If not all globs
+            matched a component, `None` is returned.
+        '''
+        def concat(strs): return ''.join(strs)
+        def f(path):
+            def matches_glob(component):
+                for glob in component_globs:
+                    if fnmatch(component, glob + '*'):
+                        return glob
+                return None
+            #   Convert globs_and_nones to a list now because we use it
+            #   several times and otherwise we'd need to reset the iterator.
+            globs_and_nones = list(map(matches_glob, pathcomponents(path)))
+            matched_globs = set(globs_and_nones) - set([None])
+            if matched_globs != set(component_globs):
+                raise MatchingPath.AllGlobsMustMatch()
+                return None # Not all matched
+            sortkey = reverse(concat(
+                map(lambda x: '0' if x else '1', globs_and_nones)))
+                #   Match (0) should sort before non-match(1)
+            return sortkey
+        return f
+
+    def assertComparable(self, other):
+        if self.cglobs != other.cglobs:
+            raise self.Incomparable('different component_globs')
+
+    def __eq__(self, other):
+        self.assertComparable(other)
+        return self.path == other.path
+
+    def __lt__(self, other):
+        self.assertComparable(other)
+        return (self.sortkey, self.path) < (other.sortkey, other.path)
+
+
+def matchandsort(component_globs):
+    ''' Taking a list of component_globs (each with implied `*` at end) and
+        (curried) list of project_paths, return a sorted list of MatchingPaths.
+    '''
+    def notNone(x): return x is not None
+    def f(project_paths):
+        return sorted(filter(notNone,
+                map(MatchingPath.constructor(component_globs), project_paths)))
+    return f
 
 def main():
     parser = ArgumentParser(description='''
@@ -63,15 +128,15 @@ def main():
         Return list of matching project paths separated by null char.
         (Use `read -d $'\\0' to read these.)
         ''')
-    parser.add_argument('component_glob', nargs='*',
+    parser.add_argument('component_globs', nargs='*',
         help='glob patterns to match components in paths')
     args = parser.parse_args()
 
     separator = '\n'
     if args.complete_words: separator = '\0'
     pathss = map(expand_pdglob, readconfig())
-    for match in flatten(map(pdmatches(args.component_glob), pathss)):
-        sys.stdout.write(match)
+    for match in flatten(map(matchandsort(args.component_globs), pathss)):
+        sys.stdout.write(match.path)
         sys.stdout.write(separator)
 
 if __name__ == '__main__':
--- t/t100.py
+++ t/t100.py
@@ -1,45 +1,19 @@
+import pytest
+
 from cpd import *
+
 
 def test_flatten():
     assert ['a', 1, 'b', 2] == flatten([['a', 1], ['b', 2]])
+
+def test_reverse():
+    assert    [3,2,1] == reverse([1,2,3])
 
 def test_expand_pdglob():
     #   This relies on CWD being set to the dir above `t/`
     #   and $HOME being set to `t/home` in this project.
     assert 't/home/p1/abc' in expand_pdglob('t/home/*/*')
     assert expand_pdglob('~/p?')[1][-9:] == 't/home/p2'
-
-def test_pdmatches():
-    p1  = '/projects/one'
-    p3  = '/projects/three'
-    hp1 = '/home/foo/projects/one'
-    hp2 = '/home/foo/projects/two'
-    pps = [ p1, p3, hp1, hp2 ]
-
-    def pdm(globs, pps): return list(pdmatches(globs)(pps))
-
-    assert pdm(['no-match'], pps) == []
-    assert pdm(['on']      , pps) == [ p1, hp1 ]
-    assert pdm(['ho']      , pps) == [ hp1, hp2 ]
-    assert pdm(['on', 'ho'], pps) == [ hp1 ]
-    assert pdm(['on', 'tw'], pps) == [ ]
-
-def test_glob_match():
-    path = '/foo/bar/baz/quux'  # No 'g's in here!
-    matches = [ '', '*',
-        'foo', 'f',
-        'b', 'bar', 'b*', 'bar*',
-        'quu', '*quu',
-        '?a', '[abc]a',
-        ]
-    non_matches = [
-        'g', '*g', 'x*', 'quuxz*', '?b', '[ac]a',
-        '/', # We don't currently support matches across multiple components.
-        ]
-    for m in matches:
-        assert glob_match(path, m)
-    for n in non_matches:
-        assert not glob_match(path, n)
 
 def test_pathcomponents():
     p = pathcomponents
@@ -56,3 +30,92 @@ def test_pathcomponents():
     assert p('/foo/bar/')       == ['foo', 'bar', '']
     assert p('/foo///bar/')     == ['foo', 'bar', '']
     assert p('/a/b/c/d/e')      == ['a', 'b', 'c', 'd', 'e']
+
+@pytest.mark.parametrize('globs, path, rkey', [
+    #   Sort key is '0' for match, '1' for not, reversed
+    (['a'       ],  '/charlie/bravo/alpha',     '011'),
+    (['a'       ],  '/alpha/bravo/alpha',       '010'),
+    (['b'       ],  '/bravo/bravo/bravo',       '000'),
+    (['a', 'b'  ],  '/bravo/alpha/charlie',     '100'),
+    (['a'       ],  '/foo/bravo/alpha',         '011'),
+    (['b'       ],  '/bravo/bravo/bravo',       '000'),
+    (['a', 'b'  ],  '/bravo/alpha/charlie',     '100'),
+
+    pytest.param(['a', 'al' ], '/bravo/alpha/charlie', '101',
+        marks=pytest.mark.xfail(strict=True, reason='bug: both globs match'
+            ' but we return just the first glob that matched, internally')),
+
+    #   Glob testing
+    (['',       ],  '/alpha/bravo/charlie',     '111'), # XXX no matches?
+    (['?r',     ],  '/alpha/bravo/charlie',     '101'),
+    (['??a',    ],  '/alpha/bravo/charlie',     '001'),
+    (['a*p',    ],  '/alpha/bravo/charlie',     '110'),
+    (['[ab]',   ],  '/alpha/bravo/charlie',     '100'),
+    (['[ac]',   ],  '/alpha/bravo/charlie',     '010'),
+    (['*[xyz]'  ],  '/nope/yes/aaaz/abxcd',     '0001'),
+])
+def test_makesortkey(globs, path, rkey):
+    assert (rkey, path) == (MatchingPath.makesortkey(globs)(path), path)
+
+def test_makesortkey_notallmatch():
+    inputs = [
+        (['x', 'b'],    '/bravo/alpha/charlie'),
+        (['a'],         '/bravo/bravo/bravo'),
+        (['a*b'],       '/alpha/bravo'),
+    ]
+    for globs, path in inputs:
+        with pytest.raises(MatchingPath.AllGlobsMustMatch):
+            MatchingPath.makesortkey(globs)(path)
+
+def mp(path):
+    ''' Our standard test MatchingPath with a fixed glob.
+        Matching:     bravo, delta
+        Non-matching: alpha, charlie, echo
+    '''
+    return MatchingPath(path, ['b', 'd'])
+
+def test_MatchingPath_construction():
+    ''' A MatchingPath must always have at least one component matching
+        each glob. This is an integration test; the full test of the
+        check is test_MatchingPath_makesortkey_notallmatch.
+    '''
+    assert mp('/charlie/bravo/delta')
+    with pytest.raises(MatchingPath.AllGlobsMustMatch):
+        assert mp('/charlie/bravo/echo')
+
+def test_MatchingPath_ordering():
+    #   XXX This test assumes that all component_globs match a component!
+
+    with pytest.raises(MatchingPath.Incomparable):
+        mp('/bravo/delta') == MatchingPath('/bravo/delta', ['b', 'de'])
+    with pytest.raises(MatchingPath.Incomparable):
+        mp('/bravo/delta') < MatchingPath('/bravo/delta', ['b', 'de'])
+
+    assert mp('/bravo/delta') == mp('/bravo/delta')
+    assert mp('/bravo/delta') != mp('/bravo/delta/charlie')
+
+    #   If the indices of the matching components are the same for
+    #   both, use lexical order.
+    assert mp('/bravo/delta')       < mp('/delta/bravo')
+    assert mp('/alpha/bravo/delta') < mp('/echo/bravo/delta')
+    assert mp('/echo/bravo/delta')  < mp('/echo/delta/bravo')
+
+    #   If matching component is more to the right, that path sorts first.
+    #   I.e., We want to sort matches deeper in the path before those
+    #   closer to the beginning because we're thinking first about our
+    #   project names and then about how the projects are grouped at
+    #   higher level.
+    assert mp('/echo/bravo/delta')          < mp('/bravo/delta/echo')
+    assert mp('/echo/bravo/delta/foxtrot')  < mp('/bravo/delta/echo/foxtrot')
+    assert mp('/echo/bravo/delta')          < mp('/bravo/delta/echo/foxtrot')
+    assert mp('/echo/bravo/delta')          < mp('/alpha/bravo/delta/foxtrot')
+    assert mp('/alpha/foxtrot/bravo/delta') < mp('/bravo/delta/echo')
+    assert mp('/bravo/bravo/delta')         < mp('/bravo/echo/bravo/delta')
+
+def test_MatchingPath_constructor():
+    m = MatchingPath.constructor
+    assert     m(['a'       ])('/foo/bravo/alpha')
+    assert not m(['a'       ])('/bravo/bravo/bravo')
+    assert     m(['b'       ])('/bravo/bravo/bravo')
+    assert     m(['a', 'b'  ])('/bravo/alpha/charlie')
+    assert not m(['x', 'b'  ])('/bravo/alpha/charlie')
